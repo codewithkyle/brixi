6a570cd4cc19e9114ad2cf4d67dccefe9effd648
Kyle Andrews <kylea@page.works>
2020-03-24 07:02:22 -0400

    Project setup

---
+++ cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const fs = require("fs");
+const path = require("path");
+const glob = require("glob");
+const semver = require("semver");
+const sass = require("node-sass");
+const yargs = require("yargs").argv;
+const minify = require("minify");
+const ora = require("ora");
+
+const cwd = process.cwd();
+
+/** Verify Nodejs version */
+const packageJson = require(path.join(__dirname, "package.json"));
+const version = packageJson.engines.node;
+if (!semver.satisfies(process.version, version)) {
+    const rawVersion = version.replace(/[^\d\.]*/, "");
+    console.log(`Brixi requires at least Node v${rawVersion} and you're using Node ${process.version}`);
+    process.exit(1);
+}
